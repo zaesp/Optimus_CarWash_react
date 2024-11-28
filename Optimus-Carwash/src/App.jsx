dbf4382e4379cf73723f8f3f8824f1c479fc9955
zaesp <francoyupanqui04@gmail.com>
2024-11-27 22:21:59 -0500

    Nuevo proyecto

---
+++ Optimus-Carwash/src/App.jsx
@@ -0,0 +1,13 @@
+//pantalla principal (acaba ba el main el header el footer)
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; //npm i react-r
+
+//importar todos los componentes(vistas) que se veran 
+
+
+const App = () => {
+  return (
+    <div>Optimus Carwash</div>
+  )
+}
+
+export default App
